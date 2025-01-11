ea8a46a723e464d4c7c1efaee87e08cdd44b065a
Christopher Aubut <christopher@aubut.me>
2025-01-10 21:51:15 -0700

    chore: save

--- packages/girs-fixup/bin/girs-fixup.ts
+++ packages/girs-fixup/bin/girs-fixup.ts
@@ -1,49 +1,94 @@
 #!/usr/bin/env bun
 
-import { runMain } from '@astal-config/girs-fixup';
+import { Command, HelpDoc, Options, Span, ValidationError } from '@effect/cli';
+import { FileSystem, Terminal } from '@effect/platform';
+import { BunContext, BunRuntime, BunTerminal } from '@effect/platform-bun';
+import { Ansi, AnsiDoc } from '@effect/printer-ansi';
+import { lineBreak } from '@effect/printer/Doc';
+import { Array, Console, Effect, flow, Number, pipe, Record, String } from 'effect';
+import { max } from 'effect/Number';
+// import { Array, Console, Effect, flow, Inspectable, pipe } from 'effect';
 
-runMain();
-// import { Command, HelpDoc, Options as CliOptions } from '@effect/cli';
-// import { Command as PlatformCommand, FileSystem, Path } from '@effect/platform';
-// import { BunContext, BunRuntime } from '@effect/platform-bun';
-// import { Doc } from '@effect/printer';
-// import { Array, Console, Data, Effect, pipe } from 'effect';
-//
+// import { PackageName } from '@astal-config/girs-fixup';
+
 // import packageJson from '../package.json' with { type: 'json' };
-//
-// /**
-//  * Generic error for all build errors using a `HelpDoc` for prettier messages.
-//  */
-// export class FixupError extends Data.TaggedError('FixupError')<{
-//   error: HelpDoc.HelpDoc;
-// }> {}
-//
-// export interface PackageName {
-//   ext: 'd.ts';
-//   packageName: string;
-//   scope: 'astal' | 'girs';
-// }
-//
-// export namespace PackageName {
-//   export const make = (filename: string): Effect.Effect<PackageName> => {
-//     const { ext, packageName, scope } = {
-//       .../^(?<scope>astal)?(?<packageName>.+)\.(?<ext>d\.ts)$/.exec(filename)?.groups,
-//     } as {
-//       ext: 'd.ts';
-//       packageName: string;
-//       scope?: 'astal';
-//     };
-//     return { ext, packageName, scope: scope ?? 'girs' };
-//   };
-// }
-//
+
+const { annotate, cat, cats, char,line, colon, empty, hang, hsep, indent, reflow, space, hcat, spaces, text, vsep } = AnsiDoc;
+
+const b = flow(annotate(Ansi.bold));
+
+const red = flow(annotate(Ansi.red));
+const gray = flow(annotate(Ansi.white));
+const seal$ = flow(AnsiDoc.render({ style: 'pretty' }));
+const seal = flow(seal$, text);
+
+
+// const prop = (k: AnsiDoc.AnsiDoc, v: AnsiDoc.AnsiDoc) =>
+//   hsep([k, pipe(v, AnsiDoc.nest(4), annotate(Ansi.red))]);
+
+const t = Effect.runSync(
+  Terminal.Terminal.pipe(
+    Effect.flatMap(({ columns }) => columns),
+    Effect.provide(BunTerminal.layer),
+  ),
+);
+
+const bold = (message: string, props?: Record<string, string>, extra?: string) => {
+  const maxKeyLength = pipe(Record.keys(props ?? {}), Array.map(String.length), Array.reduce(0, Number.max));
+  return cats([
+    hsep([text('× Error:'), pipe(reflow(message), hang(0))]),
+    empty,
+    pipe(
+      Record.toEntries(props ?? {}),
+      Array.map(([key, value]) =>
+        AnsiDoc.hsep([spaces(maxKeyLength - key.length), text(`${key}:`), pipe(reflow(value), hang(0))]),
+      ),
+      vsep,
+    ),
+    extra ? cat(line, text(extra)) : empty,
+    // pipe(AnsiDoc.vsep([
+    //   AnsiDoc.text('Filename'),
+    //   AnsiDoc.text('Another: asdf'),
+    // ]),
+    // AnsiDoc.indent(2)
+    // )
+  ]);
+};
+// AnsiDoc.annotate(Ansi.bold),
+// AnsiDoc.annotate(Ansi.redBright),
+
+Effect.runSync(
+  pipe(
+    bold(
+      'Something happeneSomething happeneSomething happeneSomething happeneSomething happeneSomething happene Something happenedddddddSomething happened',
+      {
+        file: 'something.d.ts',
+        formatting:
+          'Something happeneSomething happeneSomething happeneSomething happeneSomething happeneSomething happene Something happenedddddddSomething happened',
+      },
+      "asdf"
+    ),
+    AnsiDoc.render({ options: { lineWidth: t }, style: 'pretty' }),
+    Console.log,
+  ),
+);
+
 // /**
 //  * Define the `--girs` path option.
 //  */
-// const girs = CliOptions.directory('girs', { exists: 'yes' }).pipe(
-//   CliOptions.mapEffect((tsconfig) => Path.Path.pipe(Effect.map((path) => path.resolve(tsconfig)))),
-//   CliOptions.withDefault(`${process.cwd()}/vendor/@girs`),
-//   CliOptions.withDescription(
+// const ls = Effect.map(
+//   FileSystem.FileSystem,
+//   (fs) => (path: string) =>
+//     fs
+//       .readDirectory(path)
+//       .pipe(
+//         Effect.mapError((error) => ValidationError.invalidValue(HelpDoc.p(Inspectable.stringifyCircular(error, 2)))),
+//       ),
+// );
+//
+// const girs = Options.directory('girs', { exists: 'yes' }).pipe(
+//   Options.withDefault(`${process.cwd()}/vendor/@girs`),
+//   Options.withDescription(
 //     pipe(
 //       Doc.vsep([
 //         Doc.text('Location of @girs generated types.'),
@@ -55,19 +100,20 @@ runMain();
 //       }),
 //     ),
 //   ),
+//   Options.mapEffect((girs) =>
+//     pipe(
+//       ls,
+//       Effect.ap(Effect.succeed(girs)),
+//       Effect.flatten,
+//       Effect.flatMap(flow(Array.map(PackageName.fromDts), Effect.all)),
+//     ),
+//   ),
 // );
 //
 // /**
 //  * App entry point.
 //  */
-// const command = Command.make('girs-fixup', { girs }, ({ girs }) =>
-//   Effect.Do.pipe(
-//     Effect.bind('fs', () => FileSystem.FileSystem),
-//     Effect.flatMap(({ fs }) => fs.readDirectory(girs)),
-//     Effect.map(Array.map(PackageName.make)),
-//     Effect.flatMap(Console.log),
-//   ),
-// );
+// const command = Command.make('girs-fixup', { girs }, Console.log);
 //
 // /**
 //  * Set app metadata.
@@ -84,6 +130,6 @@ runMain();
 //   Effect.provide(BunContext.layer),
 //   BunRuntime.runMain({
 //     // use our own printer
-//     // disableErrorReporting: true,
+//     disableErrorReporting: true,
 //   }),
 // );
--- packages/girs-fixup/src/PackageName.ts
+++ packages/girs-fixup/src/PackageName.ts
@@ -1,21 +1,39 @@
 import { HelpDoc, ValidationError } from '@effect/cli';
-import { Effect, ParseResult, Schema as S } from 'effect';
+import { Path } from '@effect/platform';
+import { Effect, ParseResult, pipe, Schema as S } from 'effect';
 
 export const PackageName = S.Struct({
+  absolutePath: S.URLFromSelf,
   ext: S.Literal('d.ts'),
-  packageName: S.NonEmptyTrimmedString,
+  module: S.NonEmptyTrimmedString,
   scope: S.optionalWith(S.Literal('astal', 'girs'), { default: () => 'girs' }),
 });
 
 export interface PackageName extends S.Schema.Type<typeof PackageName> {}
 
-export const make = (filename: string): Effect.Effect<PackageName, ValidationError.ValidationError> =>
-  S.decodeUnknown(PackageName)(/^(?<scope>astal)?(?<packageName>.+)\.(?<ext>d\.ts)$/.exec(filename)?.groups).pipe(
-    Effect.catchAll(({ issue }) =>
-      ParseResult.TreeFormatter.formatIssue(issue).pipe(
-        Effect.map((error) => HelpDoc.sequence(HelpDoc.p(`Filename: ${filename}`), HelpDoc.p(error))),
-        Effect.map(ValidationError.invalidValue),
-        Effect.flip,
-      ),
+export const fromDts = (file: string): Effect.Effect<PackageName, ValidationError.ValidationError> =>
+  Effect.Do.pipe(
+    Effect.bind('path', () => Path.Path),
+    Effect.bind('filename', ({ path }) => Effect.succeed(path.basename(file))),
+    Effect.bind('absolutePath', ({ path }) => pipe(path.normalize(file), path.toFileUrl)),
+    Effect.flatMap(({ absolutePath, filename }) =>
+      S.decodeUnknown(PackageName)({
+        absolutePath,
+        .../^(?<scope>astal)?(?<module>.+)\.(?<ext>d\.ts)$/.exec(filename)?.groups,
+      }),
     ),
+    (v) => v,
+    Effect.catchTags({
+      BadArgument: (error) => 
+          HelpDoc.sequence(HelpDoc.p(`File: ${file}`), HelpDoc.p(error))),
+          // Effect.map(ValidationError.invalidValue),
+          // Effect.flip,
+        //),
+      ParseError: ({ issue }) =>
+        ParseResult.TreeFormatter.formatIssue(issue).pipe(
+          Effect.map((error) => HelpDoc.sequence(HelpDoc.p(`File: ${file}`), HelpDoc.p(error))),
+          Effect.map(ValidationError.invalidValue),
+          Effect.flip,
+        ),
+    }),
   );
